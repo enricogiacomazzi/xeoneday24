ead97dd87b8872cfbb145c78897aa215b24af2e6
Enrico <e.giacomazzi@contralto-audio.com>
2024-05-16 14:27:22 +0200

    demo styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import { useState } from 'react'
-
+import clsx from 'clsx';
+import { css } from '@emotion/css';
 
 export default function App() {
+  
   const [count, setCount] = useState(0);
+  
+
+
+  const titleStyle = clsx(
+    'title', 
+    count >= 5 && 'danger', 
+    {'very-danger': count >= 10}
+  );
+
+
+
+
 
   function inc() {
     setCount(count + 1);
@@ -18,13 +32,29 @@ export default function App() {
     setCount(0);
   }
 
+
+
   return (
     <>
-      <h1>{count}</h1>
+      <h1 className={titleStyle}>
+        {count}
+      </h1>
+
+      <div className={css`
+          height: 15px;
+          background-color: purple;
+          width: ${count * 10}px;
+      `}/>
+      
       <button onClick={inc}>+</button>
       <button onClick={dec}>-</button>
-      <button onClick={reset}>reset</button>
+      <button onClick={reset}>
+        reset
+      </button>
     </>
   )
+
+
+
 }
 
